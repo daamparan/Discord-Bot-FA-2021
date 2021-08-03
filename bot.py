82bfa25e87364eca101a65e51d4e9c6ec2209cb4
daamparan <43897297+daamparan@users.noreply.github.com>
2021-08-02 18:29:54 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import os
 import discord #access all API calls for discord 
+from dotenv import load_dotenv
 
-from dotnev import load_dotenv
-
-load_dotnev()
+load_dotenv()
 TOKEN = os.getenv('DISCORD TOKEN') #Authentication token for the server
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
