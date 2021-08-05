b3630bc41a078c3edaf83e49c8e12f7ae854a34f
daamparan <43897297+daamparan@users.noreply.github.com>
2021-08-05 12:56:24 -0600

    Updated .env & bot.py file w/ None Type bug corrected

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ import discord #access all API calls for discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD TOKEN') #Authentication token for the server
+TOKEN = os.getenv('DISCORD_TOKEN') #Authentication token for the server
 
 client = discord.Client()
 
 @client.event
-async def on_ready():
+async def on_ready(): #will track the connection event; once ran it is ready for further processing
     print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
