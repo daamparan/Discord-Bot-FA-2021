7932e7e8d71b56b3f745ee94ab16043c53121ecf
daamparan <43897297+daamparan@users.noreply.github.com>
2021-08-12 21:10:58 -0600

    Update bot.py
    Update including -
       - Bug w/ establishing discord connection
       - Basic additions of guild connections including printing Bot Name and Server Name

--- bot.py
+++ bot.py
@@ -3,12 +3,23 @@ import discord #access all API calls for discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN') #Authentication token for the server
+TOKEN = os.getenv('DISCORD_TOKEN') #Authentication token for the bot 
+GUILD = os.getenv('DISCORD_GUILD') #Authentication used for the server | server name
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready(): #will track the connection event; once ran it is ready for further processing
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds: #go thorugh the servers bots are added for
+        if guild.name == GUILD: #once it matches with one of the servers in .env 
+            break
+    print(f'\n{client.user} has connected to Discord!')
+    print(f'{guild.name}(id: {guild.id})')
+
+    members = '\n - '.join([member.name for member in guild.members])
+
+    print(f'Guild Members:\n - {members}')
+
 
 client.run(TOKEN)
